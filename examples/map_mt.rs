8b9692a873bca66f6bbd7067dc263da9bff0a6b9
Vasco Costa <vascomacosta@gmail.com>
2025-03-02 16:09:22 +0000

    Update map_mt example

--- examples/map_mt.rs
+++ examples/map_mt.rs
@@ -1,15 +1,77 @@
-use std::{
-    cmp,
-    sync::{mpsc::Sender, Arc},
-    thread,
-};
+mod parallel_map {
+    use std::{
+        cmp,
+        sync::{mpsc::Sender, Arc},
+        thread,
+    };
 
-const DEFAULT_NUM_THREADS: usize = 4;
-const THREAD_MUL: usize = 4;
+    const DEFAULT_NUM_THREADS: usize = 4;
+    const THREAD_MUL: usize = 4;
+
+    pub trait ParallelMap<T, F>
+    where
+        T: Copy + Send + Sync + 'static,
+        F: Fn(T) -> T + Copy + Send + Sync + 'static,
+    {
+        fn map(
+            self,
+            num_threads: Option<usize>,
+            transform: F,
+            tx: Sender<T>,
+        ) -> Result<(), &'static str>;
+    }
+
+    impl<T, F> ParallelMap<T, F> for Vec<T>
+    where
+        T: Copy + Send + Sync + 'static,
+        F: Fn(T) -> T + Copy + Send + Sync + 'static,
+    {
+        fn map(
+            self,
+            num_threads: Option<usize>,
+            transform: F,
+            tx: Sender<T>,
+        ) -> Result<(), &'static str> {
+            let num_threads = match num_threads {
+                Some(num_threads) => num_threads,
+                None => match thread::available_parallelism() {
+                    Ok(num_threads) => num_threads.get(),
+                    Err(_) => DEFAULT_NUM_THREADS,
+                },
+            };
+
+            let elements = Arc::new(self);
+            let elements_len = elements.len();
+
+            for n in 0..cmp::min(num_threads * THREAD_MUL, elements_len) {
+                let elements_clone = Arc::clone(&elements);
+                let tx_clone = tx.clone();
+
+                let window = elements_len.div_ceil(num_threads * THREAD_MUL);
+
+                thread::spawn(move || -> Result<(), &'static str> {
+                    let start = n * window;
+                    let end = cmp::min((n * window) + window, elements_len);
+
+                    for element in &elements_clone[start..end] {
+                        tx_clone
+                            .send(transform(*element))
+                            .map_err(|_| "Could not send to channel")?;
+                    }
+
+                    Ok(())
+                });
+            }
+
+            Ok(())
+        }
+    }
+}
+
+use parallel_map::ParallelMap;
 
 fn sum_of_divisors(n: usize) -> usize {
     let mut sum = 0;
-
     for i in 1..=n {
         let mut count = 0;
         for j in 1..=(i as f64).sqrt() as usize {
@@ -22,57 +84,17 @@ fn sum_of_divisors(n: usize) -> usize {
         }
         sum += count;
     }
-
     sum
-}
-
-fn map_mt<T>(
-    num_threads: usize,
-    elements: Arc<Vec<T>>,
-    transform: fn(T) -> T,
-    tx: Sender<T>,
-) -> Result<(), &'static str>
-where
-    T: Copy + Send + Sync + 'static,
-{
-    let elements_len = elements.len();
-    for n in 0..cmp::min(num_threads, elements_len) {
-        let elements_clone = Arc::clone(&elements);
-        let tx_clone = tx.clone();
-        let window = elements_len.div_ceil(num_threads);
-        thread::spawn(move || -> Result<(), &'static str> {
-            let start = n * window;
-            let end = cmp::min((n * window) + window, elements_len);
-            for element in &elements_clone[start..end] {
-                tx_clone
-                    .send(transform(*element))
-                    .map_err(|_| "Could not send to channel")?;
-            }
-
-            Ok(())
-        });
-    }
-
-    Ok(())
 }
 
 fn main() -> Result<(), &'static str> {
     let (tx, rx) = std::sync::mpsc::channel();
+    let elements: Vec<usize> = (1..=5000).collect();
 
-    let num_threads = match thread::available_parallelism() {
-        Ok(num_threads) => num_threads.get(),
-        Err(_) => DEFAULT_NUM_THREADS,
-    };
-    let elements = (1..=5000).collect();
-    map_mt(
-        num_threads * THREAD_MUL,
-        Arc::new(elements),
-        sum_of_divisors,
-        tx,
-    )?;
+    elements.map(None, sum_of_divisors, tx).unwrap();
 
-    while let Ok(n) = rx.recv() {
-        println!("{n}");
+    while let Ok(value) = rx.recv() {
+        println!("{}", value);
     }
 
     Ok(())

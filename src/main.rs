b133b46583d51040e5d6aeffb2b6af56cf7450a3
Vasco Costa <vascomacosta@gmail.com>
2026-03-08 16:44:47 +0000

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,27 @@
+enum Token {
+    Number(i64),
+    Plus,
+    Star,
+}
+
+enum Expr {
+    Number(i64),
+}
+
+struct BinaryOp {
+    left: Expr,
+    op: Operator,
+    right: Expr,
+}
+
+enum Operator {
+    Add,
+    Mul,
+}
+
+struct Parser {
+    tokens: Vec<Token>,
+    pos: usize,
+}
+
 fn main() {}
